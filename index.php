8ca2e36591f3984fbffb8f4cfb0b4cad2aadbaf4
Lip-7 <zhighofficial@gmail.com>
2023-05-05 15:24:46 +0200

    minor graphics fixes

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
       <h1 class="text-8xl text-shadows font-black">Password</h1>
       <h1 class="text-8xl text-shadows font-black">Generator</h1>
     </div>
-    <form action="" class="w-full mx-auto rounded-lg bg-white shadow p-5 text-gray-800" style="max-width: 500px">
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" class="w-full mx-auto rounded-lg bg-white shadow p-5 text-gray-800" style="max-width: 500px">
       <div class="mb-2">
         <label class="block text-xs font-semibold text-gray-500 mb-2">PASSWORD LENGTH</label>
-        <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="Min 8 - Max 32" type="number" min="8" max="32" id="passLenghtInput" />
+        <input class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors" placeholder="Min 8 - Max 32" type="number" min="8" max="32" id="passLenghtInput" name="passLenght" required/>
         <input class="w-full" type="range" min="8" max="32" id="passLenghtRange">
       </div>
-      <div class="flex -mx-2 mb-2">
+      <div class="flex -mx-2 mb-2 text-center">
         <div class="w-1/2 px-2">
           <label for="charsLower">
             <input type="checkbox" class="align-middle" id="charsLower" checked>
@@ -43,7 +43,7 @@
           </label>
         </div>
       </div>
-      <div class="flex -mx-2">
+      <div class="flex -mx-2 text-center">
         <div class="w-1/2 px-2">
           <label for="charsNumeric">
             <input type="checkbox" class="align-middle" id="charsNumeric" checked>
@@ -55,6 +55,11 @@
             <input type="checkbox" class="align-middle" id="charsSymbols" checked>
             <span class="text-xs font-semibold text-gray-500">SYMBOLS</span>
           </label>
+        </div>
+      </div>
+      <div class="-mx-2 mt-2">
+        <div class="px-2 flex justify-center">
+          <input type="submit" class="align-middle cursor-pointer px-3 py-2 rounded-full" id="formSub" value="Create Password">
         </div>
       </div>
     </form>
